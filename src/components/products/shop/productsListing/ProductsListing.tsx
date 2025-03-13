e56f305c6c84d9ce64e707ea52ad6b21e5373777
pavitech <pavitechjbr@gmail.com>
2025-03-12 21:55:39 -0300

    feat: cart system logic implemented

--- src/components/products/shop/productsListing/ProductsListing.tsx
+++ src/components/products/shop/productsListing/ProductsListing.tsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CloseIcon from "../../../../assets/icons/close.png";
+import SearchIcon from "../../../../assets/icons/search-icon.png";
 import { IProduct } from "../../../../interfaces/product.interface";
 import { Pagination } from "../../pagination/Pagination";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,9 +17,17 @@ import { ProductCard } from "../../../cards/ProductCard";
 export function ProductsListing() {
   const dispatch = useDispatch<AppDispatch>();
 
-  const { products, totalProducts, filter, price, page, priceEndPoints } = useSelector(
-    (state: RootState) => state.products
-  );
+  const {
+    products,
+    totalProducts,
+    filter,
+    titleFilter,
+    price,
+    page,
+    priceEndPoints,
+  } = useSelector((state: RootState) => state.products);
+
+  const [titleFilterInput, setTitleFilterInput] = useState("");
 
   const handleCloseFilters = () => {
     dispatch(setFilters(""));
@@ -26,7 +35,11 @@ export function ProductsListing() {
 
   const handleResetPriceFilter = () => {
     dispatch(setPrice(priceEndPoints.max));
-  }
+  };
+
+  const handleTitleFilter = () => {
+    dispatch(setFilters(titleFilter));
+  };
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -40,24 +53,42 @@ export function ProductsListing() {
         <div className="flex flex-col gap-3">
           <h5>Applied Filter:</h5>
           <div className="flex gap-3">
-            {filter && (
-              <div
-                className="flex gap-2 px-4 py-0.5 rounded-full border border-borderColor cursor-pointer"
-                onClick={handleCloseFilters}
-              >
-                <p className="text-neutral">{filter}</p>
-                <img src={CloseIcon} alt="close" />
+            <div>
+              {filter && (
+                <div
+                  className="flex gap-2 px-4 py-0.5 rounded-full border border-borderColor cursor-pointer"
+                  onClick={handleCloseFilters}
+                >
+                  <p className="text-neutral">{filter}</p>
+                  <img src={CloseIcon} alt="close" />
+                </div>
+              )}
+              {price < priceEndPoints.max && (
+                <div
+                  className="flex gap-2 px-4 py-0.5 rounded-full border border-borderColor cursor-pointer"
+                  onClick={handleResetPriceFilter}
+                >
+                  <p className="text-neutral">${price}.00</p>
+                  <img src={CloseIcon} alt="close" />
+                </div>
+              )}
+            </div>
+            <div className="w-full">
+              <div className="relative w-fit ml-auto">
+                <input
+                  className="float-right w-64.5 pl-12 font-medium text-sm"
+                  placeholder="Search products"
+                  value={titleFilterInput}
+                  onChange={(e) => setTitleFilterInput(e.target.value)}
+                />
+                <img
+                  src={SearchIcon}
+                  alt="close"
+                  className="absolute left-2 top-2.5 w-6 h-6"
+                  onClick={handleTitleFilter}
+                />
               </div>
-            )}
-            {price < priceEndPoints.max && (
-              <div
-                className="flex gap-2 px-4 py-0.5 rounded-full border border-borderColor cursor-pointer"
-                onClick={handleResetPriceFilter}
-              >
-                <p className="text-neutral">${price}.00</p>
-                <img src={CloseIcon} alt="close" />
-              </div>
-            )}
+            </div>
           </div>
           <p className="text-vividBlack py-5.5">
             Showing {page}-{products.length} of {totalProducts} results.
